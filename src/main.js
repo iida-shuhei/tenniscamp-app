c67ff9a2e8afa9a1af345612f53d0c31ee5c897d
IidaShuhei <shuhei.iida@rakus-partners.co.jp>
2020-07-30 21:32:34 +0900

    a

--- src/main.js
+++ src/main.js
@@ -4,11 +4,26 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import axios from "axios";
-// import firebase from "firebase";
+import firebase from "firebase";
 
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
 axios.defaults.baseURL = process.env.VUE_APP_API_BASE_URL;
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyA0Y7fTVM3NX5XRmvducfJd-QS8vGiLI9M",
+  authDomain: "tenniscamp-app.firebaseapp.com",
+  databaseURL: "https://tenniscamp-app.firebaseio.com",
+  projectId: "tenniscamp-app",
+  storageBucket: "tenniscamp-app.appspot.com",
+  messagingSenderId: "43058144599",
+  appId: "1:43058144599:web:1047c15cb5c3526cd40a12",
+  measurementId: "G-PZLMGBWS3J"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 new Vue({
   router,
