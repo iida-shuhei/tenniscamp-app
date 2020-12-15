f58a4e09a5a20e8992e6db50e7501cbaa941e139
root <tennis.yahoo@gmail.com>
2020-12-15 12:48:53 +0900

    a

--- src/main.js
+++ src/main.js
@@ -1,25 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
 import axios from "axios";
 import firebase from "firebase";
 
-Vue.config.productionTip = false
-Vue.prototype.$axios = axios
+Vue.config.productionTip = false;
+Vue.prototype.$axios = axios;
 axios.defaults.baseURL = process.env.VUE_APP_API_BASE_URL;
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyA0Y7fTVM3NX5XRmvducfJd-QS8vGiLI9M",
-  authDomain: "tenniscamp-app.firebaseapp.com",
-  databaseURL: "https://tenniscamp-app.firebaseio.com",
-  projectId: "tenniscamp-app",
-  storageBucket: "tenniscamp-app.appspot.com",
-  messagingSenderId: "43058144599",
-  appId: "1:43058144599:web:1047c15cb5c3526cd40a12",
-  measurementId: "G-PZLMGBWS3J"
+  apiKey: "AIzaSyC8pmGh-zapCO8aD95LCBwsQHfXY7QgYmo",
+  authDomain: "tenniscamp-1d8dd.firebaseapp.com",
+  projectId: "tenniscamp-1d8dd",
+  storageBucket: "tenniscamp-1d8dd.appspot.com",
+  messagingSenderId: "957925085891",
+  appId: "1:957925085891:web:6257828afc6913d0e2c876",
+  measurementId: "G-RR852J0Y1Z",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -29,5 +28,5 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
